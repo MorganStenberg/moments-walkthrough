1b90f8c1798cd29f4ce2dc1353b9fa6f6005b183
Morgan Stenberg <morganstenberg@gmail.com>
2024-03-12 15:47:18 +0000

    add axios interceptors to handle login and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://drf-api-wk-e66a91f679e3.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
