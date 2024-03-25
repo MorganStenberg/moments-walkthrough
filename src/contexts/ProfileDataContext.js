421f0f5c99da5ef4b0172b47ac05ca25e17e9de2
Morgan Stenberg <morganstenberg@gmail.com>
2024-03-25 09:54:11 +0000

    add custom util for following function for profiles

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useCurrentUser } from "./CurrentUserContext";
+import { followHelper } from "../utils/utils";
 
 
 
@@ -25,7 +26,23 @@ export const ProfileDataProvider = ({ children }) => {
         try {
           const {data} = await axiosRes.post('/followers/', {
             followed: clickedProfile.id
-          })
+          });
+
+          setProfileData((prevState) => ({
+            ...prevState,
+            pageProfile: {
+              results: prevState.pageProfile.results.map((profile) => 
+              followHelper(profile, clickedProfile, data.id)
+              ),
+            },
+            popularProfiles: {
+              ...prevState.popularProfiles,
+              results: prevState.popularProfiles.results.map((profile) => 
+              followHelper(profile, clickedProfile, data.id)
+              ),
+            },
+          }));
+
         } catch(err) {
           console.log(err)
         }
